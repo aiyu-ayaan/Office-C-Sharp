2c6d6bfd94c4eb4afa0003fd05c0ce50ad3c1580
aiyu-ayaan <aiyuayaan22@gmail.com>
2025-06-25 18:15:36 +0530

    Add project files.

---
+++ Start/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string bolString = "true";
+                bool bol = Convert.ToBoolean(bolString);
+                Console.WriteLine(bol);
+            }
+            catch (Exception ex) { 
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+        }
+    }
+}
