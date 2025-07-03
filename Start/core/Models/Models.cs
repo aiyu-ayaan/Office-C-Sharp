c5db06760cf92d251c8162bef3274b5bf48fc2b6
aiyu-ayaan <aiyuayaan22@gmail.com>
2025-07-03 18:39:38 +0530

    Refactor code structure and add new features
    - Removed the `Program` class and its `Main` method from `Program.cs`.
    - Introduced a new namespace `Oops` in `Oops.cs` with `HtttpCookies` and `Indexers` classes for cookie management.
    - Added a new namespace `Start.core.Models` in `Models.cs`, defining `UserRole` enum and `BookModel` class with properties and a constructor.
    - Created a new namespace `Start.utils` in `Utils.cs`, including `DateFormat` for date handling and `DateTimeUtils` for utility methods related to date and time.

---
+++ Start/core/Models/Models.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Start.core.Models
+{
+
+    enum UserRole
+    {
+        ADMIN,
+        TEACHER,
+        STUDENT,
+    }
+
+    public class UserModel { }
+
+  
+
+    public class BookModel {
+        private string _bookName;
+        private string _authorName;
+        private string _publisherName;
+        private int _bookId;
+        private int _bookPrice;
+        private long createdAt;
+        public BookModel(string bookName, string authorName, string publisherName,int bookPrice)
+        {
+            _bookName = bookName;
+            _authorName = authorName;
+            _publisherName = publisherName;
+            _bookPrice = bookPrice;
+            Random random = new Random();
+            _bookId = random.Next(1000, 9999);
+        }
+
+        public string BookName
+        {
+            get { return _bookName; }
+            private set {  }
+        }
+
+        public string AuthorName
+        {
+            get { return _authorName; }
+            private set { }
+        }
+
+        public string PublisherName
+        {
+            get { return _publisherName; }
+            private set {  }
+        }
+
+        public int BookId
+        {
+            get { return _bookId; }
+            private set { }
+        }
+
+        public int BookPrice
+        {
+            get { return _bookPrice; }
+            set { _bookPrice = value; }
+        }
+        public override string ToString()
+        {
+            return $"Book ID: {_bookId}, Book Name: {_bookName}, Author: {_authorName}, Publisher: {_publisherName}, Price: {_bookPrice}";
+        }
+
+    }
+}
---
+++ Start/utils/Utils.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Start.utils
+{
+
+    /***
+     * Class to handle date formats
+     * 
+     **/
+    public sealed class DateFormat
+    {
+        private static string DDMMYYYY = "dd/MM/yyyy";
+        private static string DDMYYYYHMMSSA = "dd/MM/yyyy h:mm:ss a";
+        public enum DateFormatType
+        {
+            DDMMYYYY,
+            DDMYYYYHMMSSA
+        }
+
+        private String type;
+        private DateFormat(String type)
+        {
+            this.type = type;
+        }
+
+        public string Format
+        {
+            get { 
+                if(type == null)
+                    {
+                    return DDMMYYYY; // Default format
+                }
+                return type;
+            }
+            private set { }
+        }
+
+        /**
+         * Factory method to create DateFormat instance based on type
+         * @param type DateFormatType
+         * @return DateFormat instance
+         **/
+        public static DateFormat Build(DateFormatType type){ 
+            return type switch
+            {
+                DateFormatType.DDMMYYYY => new DateFormat(DDMMYYYY),
+                DateFormatType.DDMYYYYHMMSSA => new DateFormat(DDMYYYYHMMSSA),
+                _ => new DateFormat(DDMMYYYY),
+            };
+        }
+        /**
+         * Factory method to create DateFormat instance with default format
+         * @return DateFormat instance with default format
+         * @see DateFormatType
+         **/
+        public static DateFormat Build() { 
+            return new DateFormat(DDMMYYYY);
+        }
+    }
+    public static class DateTimeUtils
+    {
+        public static long GetCurrentTimeOnMilliseconds()
+        {
+            return DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+        }
+
+        public static string FormateDate(long timestamp, DateFormat format)
+        {
+            DateTime dateTime = DateTimeOffset.FromUnixTimeMilliseconds(timestamp).UtcDateTime;
+            return dateTime.ToString(format.Format);
+        }
+        public static string FormateDate(long timestamp)
+        {
+            DateTime dateTime = DateTimeOffset.FromUnixTimeMilliseconds(timestamp).UtcDateTime;
+            return dateTime.ToString(DateFormat.Build().Format);
+        }
+    }
+}
